c04f1e204c6206f434eb5d7c3a6378e2952a6def
Nikita_Kulazhenko <Nikita_Kulazhenko@epam.com>
2016-07-21 18:26:54 +0300

    add new changes

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import "babel-polyfill";
 import React from "react";
 import {render} from "react-dom";
 import configureStore from "./store/configureStore";
+import {loadCourses} from "./actions/courseActions";
 import {Provider} from "react-redux";
 import {Router, browserHistory} from "react-router";
 import routes from "./routes";
@@ -9,6 +10,7 @@ import "./styles/styles.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 const store = configureStore();
+store.dispatch(loadCourses());
 
 render(
 	<Provider store={store}>
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,7 +1,7 @@
 import {createStore, applyMiddleware} from "redux";
 import rootReducer from "../reducers/index";
 import reduxImmutableStateInvariant from "redux-immutable-state-invariant";
-import thunk from "react-thunk";
+import thunk from "redux-thunk";
 
 export default function configureStore(initialState) {
 	return createStore(
